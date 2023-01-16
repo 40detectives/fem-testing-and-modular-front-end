00f6fa33a81607af437fe7576a0aafaead3439ce
40detectives <9024463+40detectives@users.noreply.github.com>
2023-01-16 13:22:39 +0100

    Making WebGL shaders with regl (III): refactor draw fn to use different props

--- 4-webgl/main.js
+++ 4-webgl/main.js
@@ -8,47 +8,60 @@ const icosphere = require('icosphere');
 const anormals = require('angle-normals');
 const glsl = require('glslify');
 
-const mesh = icosphere(4);
-const draw = regl({
-  frag: glsl`
-    precision highp float;
-    #pragma glslify: snoise = require('glsl-noise/simplex/4d);
-    #pragma glslify: hsl2rgb = require('glsl-hsl2rgb');
-    varying vec3 vnorm, vpos;
-    uniform float time;
-    void main() {
-      gl_FragColor = vec4((vnorm+1.0)*0.5 * vec3(0, 1, 0)
-        + hsl2rgb(snoise(vec4(vpos, time * 0.1)),1.0, 0.5)
-      , 1);
-    }
-  `,
-  vert: glsl`
-    precision highp float;
-    #pragma glslify: snoise = require('glsl-noise/simplex/4d');
-    attribute vec3 position, normal;
-    uniform mat4 projection, view;
-    varying vec3 vnorm, vpos;
-    uniform float time;
-    void main() {
-      vpos = position;
-      vnorm = normal;
-      gl_Position = projection * view 
-        * vec4(position + normal * snoise(vec4(position, time))*0.1, 1);
-    }
-  `,
-  attributes: {
-    position: mesh.positions,
-    normal: anormals(mesh.cells, mesh.positions),
-  },
-  elements: mesh.cells,
-  uniforms: {
-    time: regl.context('time')
-  },
-});
-
-regl.frame(function() {
-  regl.clear({ color: [1,0,1,1], depth: true });
-  camera(function() {
-    draw();
+function createBlob(regl) {
+  const mesh = icosphere(4);
+  return regl({
+    frag: glsl`
+      precision highp float;
+      #pragma glslify: snoise = require('glsl-noise/simplex/4d);
+      #pragma glslify: hsl2rgb = require('glsl-hsl2rgb');
+      varying vec3 vnorm, vpos;
+      uniform float time, stage;
+      void main() {
+        gl_FragColor = vec4((vnorm+1.0)*0.5 * vec3(0, 1, 0)
+          + hsl2rgb(snoise(vec4(vpos, time * 0.1 + stage)),1.0, 0.5)
+        , 1);
+      }
+    `,
+    vert: glsl`
+      precision highp float;
+      #pragma glslify: snoise = require('glsl-noise/simplex/4d');
+      attribute vec3 position, normal;
+      uniform mat4 projection, view;
+      varying vec3 vnorm, vpos;
+      uniform float time, stage;
+      uniform vec3 location;
+      void main() {
+        vpos = position;
+        vnorm = normal;
+        gl_Position = projection * view 
+          * vec4(position + location + normal * snoise(vec4(position, time + stage))*0.1, 1);
+      }
+    `,
+    attributes: {
+      position: mesh.positions,
+      normal: anormals(mesh.cells, mesh.positions),
+    },
+    elements: mesh.cells,
+    uniforms: {
+      time: regl.context('time'),
+      location: regl.prop('location'),
+      stage: regl.prop('stage'),
+    },
   });
-});
+}
+
+  const draw ={
+    blob: createBlob(regl)
+  };
+  
+  regl.frame(function() {
+    regl.clear({ color: [1,0,1,1], depth: true });
+    camera(function() {
+      draw.blob([
+        { location: [0,0,0], stage: 0},
+        { location: [0,0,-2], stage: 1},
+        { location: [0,0,2], stage: 2},
+      ]);
+    });
+  });
